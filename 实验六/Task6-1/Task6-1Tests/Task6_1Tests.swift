542debff9cacc505ec365c28bc7ca2d81530c0eb
qihuandebaiye <45944251+qihuandebaiye@users.noreply.github.com>
2018-12-18 10:15:54 +0800

    Add files via upload

---
+++ 实验六/Task6-1/Task6-1Tests/Task6_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Task6_1Tests.swift
+//  Task6-1Tests
+//
+//  Created by student on 2018/10/25.
+//  Copyright © 2018年 liqin. All rights reserved.
+//
+
+import XCTest
+@testable import Task6_1
+
+class Task6_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
