36c3bb6369fc5162f3c73ccc08597c47310e81e9
qihuandebaiye <45944251+qihuandebaiye@users.noreply.github.com>
2018-12-18 10:30:36 +0800

    Add files via upload

---
+++ 实验十三/Task13-1/Task13-1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Task13-1
+//
+//  Created by student on 2018/12/17.
+//  Copyright © 2018年 linhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sumlable: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func sumbutton(_ sender: Any) {
+        DispatchQueue.global().async {
+            var sum=0
+            for i in 1...9999999{
+                sum += i
+            }
+            DispatchQueue.main.async {
+                self.sumlable.text = "sum=\(sum)"
+            }
+        }
+       
+    }
+    
+}
+
---
+++ 实验十三/Task13-1/Task13-1Tests/Task13_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Task13_1Tests.swift
+//  Task13-1Tests
+//
+//  Created by student on 2018/12/12.
+//  Copyright © 2018年 zhangfeng. All rights reserved.
+//
+
+import XCTest
+@testable import Task13_1
+
+class Task13_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
